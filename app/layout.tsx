515e046383ea64e61785a70853388a536835d1c0
konlakarn <buapian01@gmail.com>
2026-04-16 17:27:42 +0700

    Update favico

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,12 @@ import { LanguageProvider } from "@/components/providers/language-provider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "MeawMeaw Guild Management | Where Winds Meet Thailand",
+  description: "MeawMeaw Guild Management | Where Winds Meet Thailand - Your all-in-one platform for managing your guild's operations, strategies, and member engagement in the world of Where Winds Meet. Streamline your guild management with our intuitive dashboard, comprehensive tools, and real-time updates. Join us to elevate your guild's performance and foster a thriving community in Where Winds Meet Thailand. ",
+};
 const display = Bebas_Neue({ variable: "--font-display", subsets: ["latin"], weight: "400" });
 const body = IBM_Plex_Sans_Thai({ variable: "--font-body", subsets: ["latin", "thai"], weight: ["400", "500", "700"] });
 const script = Kalam({ variable: "--font-script", subsets: ["latin"], weight: ["400", "700"] });
